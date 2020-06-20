0016b19ae5f0ab7f77b673465d1496a530b5faff
MM32250 <miguel.martinez@globallogic.com>
2020-06-20 13:49:26 -0300

    Initial commit

---
+++ src/main/java/com/mm/graphql/graphqlpoc/GraphqlpocApplication.java
@@ -0,0 +1,13 @@
+package com.mm.graphql.graphqlpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlpocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlpocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mm/graphql/graphqlpoc/GraphqlpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.graphql.graphqlpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlpocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
